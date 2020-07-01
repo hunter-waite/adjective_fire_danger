4af77e60e531db6118d0f1fac4b41fab518d984f
hunter-waite <hwaite@calpoly.edu>
2020-07-01 14:52:52 -0700

    Getting data from singular xml document

--- scripts/xml_parser.py
+++ scripts/xml_parser.py
@@ -3,7 +3,7 @@ import urllib.request
 from pprint import pprint
 
 def main():
-    url = "https://fam.nwcg.gov/wims/xsql/nfdrs.xsql?stn=44915&start=30-Jun-20&end=01-Jul-20&ndays=";
+    url = "https://fam.nwcg.gov/wims/xsql/nfdrs.xsql?stn=44915&start=30-Jun-20&end=01-Jul-20&ndays=&sort=";
 
     print("URL being used:")
     print(url)
@@ -18,11 +18,27 @@ def main():
     root = tree.getroot()
 
     for child in root:
-        temp = child.findall("adj")
-        if len(temp) == 0:
+        adj = child.find("adj")
+
+        if adj is None:
             continue;
-        for adj in temp:
-            print(adj.text)
+
+        tm = child.find("nfdr_tm")
+        dt = child.find("nfdr_dt")
+        print(adj.text, tm.text, dt.text)
+
+
+def get_dates():
+    """ Gets the dates to be used in the various URL's
+
+    Returns:
+        today: Todays date in dd-MMM-yy format
+        yesterday: Yesterdays date in dd-MMM-yy format
+    """
+    today = datetime.date.today()
+    yesterday = today - datetime.timedelta(days=1)
+
+    return today, yesterday
 
 if __name__ == "__main__":
     main()
