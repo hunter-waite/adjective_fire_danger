611bdc405449af1a1f496fe2be784bfe98cbd889
hunter-waite <hwaite@calpoly.edu>
2020-07-01 15:30:21 -0700

    Added functionality

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 Pulls and calculates adjective fire danger for San Luis Obispo County
 
 Things to do:
-1) Figure out how to pull XML data
-2) Figure out how the XML data is being mapped and possibly convert to JSON?
-3) Prosper
+1) Make *get_xml()* use the input arguments
+2) 
+3)
--- scripts/xml_parser.py
+++ scripts/xml_parser.py
@@ -4,6 +4,35 @@ import datetime
 from pprint import pprint
 
 def main():
+
+    root = get_xml(None, None, None)
+
+    for child in root:
+        adj = child.find("adj")
+
+        if adj is None:
+            continue;
+
+        tm = child.find("nfdr_tm")
+        dt = child.find("nfdr_dt")
+        print(adj.text, tm.text, dt.text)
+
+    get_dates()
+
+
+def get_xml(station, start, end):
+    """ Creates a url from the given variables to pull adjective fire rating
+    data
+
+    Args:
+        station: The number associated with the station
+        start: The start date in dd-MMM-yy format to get data from
+        end: The end date in dd-MMM-yy format to get data from
+
+    Returns:
+        root: the root of the xml tree from the data
+    """
+
     url = "https://fam.nwcg.gov/wims/xsql/nfdrs.xsql?stn=44915&start=30-Jun-20&end=01-Jul-20&ndays=&sort=";
 
     print("URL being used:")
@@ -18,17 +47,8 @@ def main():
     tree = ET.parse("xml/test.xml")
     root = tree.getroot()
 
-    for child in root:
-        adj = child.find("adj")
+    return root
 
-        if adj is None:
-            continue;
-
-        tm = child.find("nfdr_tm")
-        dt = child.find("nfdr_dt")
-        print(adj.text, tm.text, dt.text)
-
-    get_dates()
 
 def get_dates():
     """ Gets the dates to be used in the various URL's
@@ -41,6 +61,7 @@ def get_dates():
     yesterday = today - datetime.timedelta(days=1)
 
     return today, yesterday
+
 
 if __name__ == "__main__":
     main()
