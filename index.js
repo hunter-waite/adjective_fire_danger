03d0af61a45049f55b12b3da47a04e7f21ba1e86
hunter-waite <hwaite@calpoly.edu>
2020-07-01 11:37:50 -0700

    Added basic actions command

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const LA_PANZA    = 44904
 const LAS_TABLAS  = 44971
-const SLO         = 44915
+const SLO         = "44915"
 const SAN_SIMEON  = 44917
 
 function init() {
@@ -11,16 +11,43 @@ function init() {
   console.log("Today's date is: " + today);
   console.log("Yesterday's date was: " + yesterday);
 
-
+  get_xml_data(dates);
 }
 
-// gets the current date for use in the url of the xml data
+async function get_xml_data(dates) {
+  console.log("Fetching the XML for current date");
+  // url for current day
+  var url = "https://fam.nwcg.gov/wims/xsql/nfdrs.xsql?stn=" + SLO + "&start=" + dates[1] + "&end=" + dates[0] + "&ndays=";
+  var xml = await fetch(url, { mode : "no-cors"})
+                  .then(response => response.text())
+                  .then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
+                  .then(data => console.log(data))
+  console.log(url);
+  console.log(xml);
+}
+
+// gets the current date and yesterdays for use in the url of the xml data
 function curr_date() {
-  var today = new Date();   // get the date
-  yy = String(today.getFullYear()).slice(-2); // get the last two digits of the year
-  dd = String(today.getDate()).padStart(2, '0');  // day
-  mm = "None";  // get the month and convert to 3 letter code
-  switch(today.getMonth() + 1) {
+  var today = new Date();   // get todays date
+  var yesterday = new Date();
+  yesterday.setDate(today.getDate() - 1); // get yesterdays date
+
+  tyy = String(today.getFullYear()).slice(-2); // get the last two digits of the year
+  tdd = String(today.getDate()).padStart(2, '0');  // day
+  tmm = get_month_from_num(today.getMonth()+1);  // get the month and convert to 3 letter code
+
+  yyy = String(yesterday.getFullYear()).slice(-2); // get the last two digits of the year
+  ydd = String(yesterday.getDate()).padStart(2, '0');  // day
+  ymm = get_month_from_num(yesterday.getMonth()+1);  // get the month and convert to 3 letter code
+
+  today = tdd + '-' + tmm + '-' + tyy;
+  yesterday = ydd + '-' + ymm + '-' + yyy;
+  return([today, yesterday]);
+}
+
+// takes in a month number and converts to 3 letter code
+function get_month_from_num(mm) {
+  switch(mm) {
     case 1: mm = "Jan"; break;
     case 2: mm = "Feb"; break;
     case 3: mm = "Mar"; break;
@@ -34,9 +61,7 @@ function curr_date() {
     case 11: mm = "Nov"; break;
     case 12: mm = "Dec"; break;
   }
-  today = dd + '-' + mm + '-' + yy;
-  yesterday = dd-1 + '-' + mm + '-' + yy;
-  return([today, yesterday]);
+  return mm;
 }
 
 function update_danger() {
---
+++ scripts/run_test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo Pulling and parsing xml data from website
+python3 xml_parser.py
+echo Done dealing with data
