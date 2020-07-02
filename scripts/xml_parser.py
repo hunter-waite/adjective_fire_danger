e65eae85698bc7ab1c4fb90189c6e454b29d6eea
hunter-waite <hwaite@calpoly.edu>
2020-07-02 10:39:21 -0700

    Updated datetime stuff to correct for PST

--- scripts/xml_parser.py
+++ scripts/xml_parser.py
@@ -8,6 +8,8 @@ from pytz import timezone
 
 def main():
 
+    get_dates()
+
     root = get_xml(None, None, None)
 
     for child in root:
@@ -19,8 +21,6 @@ def main():
         tm = child.find("nfdr_tm")
         dt = child.find("nfdr_dt")
         print(adj.text, tm.text, dt.text)
-
-    get_dates()
 
 
 def get_xml(station, start, end):
@@ -59,21 +59,22 @@ def get_dates():
     Returns:
         today: Todays date in dd-MMM-yy format
         yesterday: Yesterdays date in dd-MMM-yy format
+        time: Current PST time
     """
-
-    today = datetime.date.today()
-    yesterday = today - datetime.timedelta(days=1)
 
     utc_current = datetime.datetime.utcnow().replace(tzinfo=pytz.utc)
     pst_zone = pytz.timezone('US/Pacific')
     pst_current = pst_zone.normalize(utc_current.astimezone(pst_zone))
-    print(pst_current)
 
-    print("Time is: ", datetime.datetime.now())
+    today = pst_current.date()
+    yesterday = today - datetime.timedelta(days=1)
+    tm = pst_current.time()
 
-    print(today, yesterday)
+    pprint("Current Date PST:" + str(today))
+    pprint("Yesterday's Date PST:" + str(yesterday))
+    pprint("Current Time PST:" + str(tm))
 
-    return today, yesterday
+    return today, yesterday, tm
 
 
 if __name__ == "__main__":
