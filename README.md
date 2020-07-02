fdbbaad05f93994dbd2ebefc5d9cef6fd14d4701
hunter-waite <hwaite@calpoly.edu>
2020-07-02 11:21:30 -0700

    Implemented real time date usage

--- README.md
+++ README.md
@@ -3,4 +3,3 @@ Pulls and calculates adjective fire danger for San Luis Obispo County
 
 Things to do:
 1) Make *get_xml()* use the input arguments
-2) Adjust time in *get_dates* to make sure it uses PST instead of UTC
--- scripts/xml_parser.py
+++ scripts/xml_parser.py
@@ -8,9 +8,26 @@ from pytz import timezone
 
 def main():
 
-    get_dates()
+    today, yesterday, tm = get_dates()
 
-    root = get_xml(None, None, None)
+    root = get_xml(44914, yesterday, today)
+    parse_xml(root)
+    root = get_xml(44904, yesterday, today)
+    parse_xml(root)
+    root = get_xml(44915, yesterday, today)
+    parse_xml(root)
+    root = get_xml(44917, yesterday, today)
+    parse_xml(root)
+
+
+def parse_xml(root):
+    """ Parses the XML tree for the adjective fire danger ratings that we want
+
+    Args:
+        root: The root of the xml tree to be looked at
+
+    """
+    print(root[0][1].text)
 
     for child in root:
         adj = child.find("adj")
@@ -28,7 +45,7 @@ def get_xml(station, start, end):
     data
 
     Args:
-        station: The number associated with the station
+        station: The number associated with the station, must be int
         start: The start date in dd-MMM-yy format to get data from
         end: The end date in dd-MMM-yy format to get data from
 
@@ -36,7 +53,8 @@ def get_xml(station, start, end):
         root: the root of the xml tree from the data
     """
 
-    url = "https://fam.nwcg.gov/wims/xsql/nfdrs.xsql?stn=44915&start=01-Jul-20&end=02-Jul-20&ndays=&sort=&type=";
+    url = "https://fam.nwcg.gov/wims/xsql/nfdrs.xsql?stn=" + str(station) + \
+          "&start=" + start + "&end=" + end +"&ndays=&sort=&type=";
 
     print("URL being used:")
     print(url)
@@ -62,19 +80,21 @@ def get_dates():
         time: Current PST time
     """
 
+    # Gets current UTC datetime object then converts to PST one
     utc_current = datetime.datetime.utcnow().replace(tzinfo=pytz.utc)
     pst_zone = pytz.timezone('US/Pacific')
     pst_current = pst_zone.normalize(utc_current.astimezone(pst_zone))
 
+    # Uses today's date to convert to yesterday's date
     today = pst_current.date()
     yesterday = today - datetime.timedelta(days=1)
     tm = pst_current.time()
 
-    pprint("Current Date PST:" + str(today))
-    pprint("Yesterday's Date PST:" + str(yesterday))
-    pprint("Current Time PST:" + str(tm))
+    # Formats the dates correctly
+    td = today.strftime("%d-%b-%y")
+    yd = yesterday.strftime("%d-%b-%y")
 
-    return today, yesterday, tm
+    return td, yd, tm
 
 
 if __name__ == "__main__":
