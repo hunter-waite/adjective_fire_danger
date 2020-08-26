76966730a882896cb24200a1959eee37d29d69f0
hunter-waite <hwaite@calpoly.edu>
2020-08-26 12:35:52 -0700

    Updated legend

--- index.js
+++ index.js
@@ -20,22 +20,26 @@ function updatePage() {
     zoomOffset: -1
   }).addTo(mymap);
 
+  // create a legend feature for the map
   var legend = L.control({ position: "bottomleft" });
 
+  // add different features for the legend
   legend.onAdd = function(mymap) {
     var div = L.DomUtil.create("div", "legend");
-    div.innerHTML += "<h4>Tegnforklaring</h4>";
-    div.innerHTML += '<i style="background: #477AC2"></i><span>Water</span><br>';
-    div.innerHTML += '<i style="background: #448D40"></i><span>Forest</span><br>';
-    div.innerHTML += '<i style="background: #E6E696"></i><span>Land</span><br>';
-    div.innerHTML += '<i style="background: #E8E6E0"></i><span>Residential</span><br>';
-    div.innerHTML += '<i style="background: #FFFFFF"></i><span>Ice</span><br>';
-    div.innerHTML += '<i class="icon" style="background-image: url(https://d30y9cdsu7xlg0.cloudfront.net/png/194515-200.png);background-repeat: no-repeat;"></i><span>Grænse</span><br>';
+    div.innerHTML += "<h4>Fire Danger</h4>";
+    div.innerHTML += '<i style="background: #FF0000"></i><span>Extreme</span><br>';
+    div.innerHTML += '<i style="background: #FF6600"></i><span>Very High</span><br>';
+    div.innerHTML += '<i style="background: #FACB00"></i><span>High</span><br>';
+    div.innerHTML += '<i style="background: #0099FF"></i><span>Moderate</span><br>';
+    div.innerHTML += '<i style="background: #00CC66"></i><span>Low</span><br>';
 
     return div;
   }
 
+  // add the legend to the map
   legend.addTo(mymap);
+
+
   // loops through all the availivble stations
                   // station name     lat, long                   image name
   for (station of [["LAS_TABLAS",     [35.656447, -120.9241],     "smokey_bear_inland"],
